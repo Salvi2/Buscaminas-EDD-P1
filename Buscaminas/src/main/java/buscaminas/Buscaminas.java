f084808f32601caa148d9d23a05b7d278623a9e3
PC <PC@Savis2>
2025-02-15 13:06:48 -0400

    Empezar Proyecto

--- Buscaminas/src/main/java/buscaminas/Buscaminas.java
+++ Buscaminas/src/main/java/buscaminas/Buscaminas.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package buscaminas;
 
-/**
- *
- * @author PC
- */
 public class Buscaminas {
 
     public static void main(String[] args) {
-        System.out.println("prueba 3!");
+        System.out.println("prueba 4 salvi!");
     }
 }
