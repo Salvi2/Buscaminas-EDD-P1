e15bd4bacb2438ad4efe08a81593c2d32d2d6196
Jesuavi <164274631+Jesuavi@users.noreply.github.com>
2025-02-15 17:40:07 -0400

    Add files via upload

---
+++ Buscaminas_EDD_1/src/buscaminas_edd_1/Buscaminas_EDD_1.java
@@ -0,0 +1,13 @@
+
+package buscaminas_edd_1;
+
+
+public class Buscaminas_EDD_1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+     
+    }
+    
+}
