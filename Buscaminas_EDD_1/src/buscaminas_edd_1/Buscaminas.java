d4394610cb8f54b3c06085fbda60e6d316fb501c
Jesus_Salvador <>
2025-02-18 13:24:13 -0400

    Acomodada la ventana de inicio Acomodada la ventana de perder FALTA GRAFOS

--- Buscaminas_EDD_1/src/buscaminas_edd_1/Buscaminas.java
+++ Buscaminas_EDD_1/src/buscaminas_edd_1/Buscaminas.java
@@ -1,73 +1,19 @@
 package buscaminas_edd_1;
- 
-import javax.swing.*;
-import java.awt.*;
+import welcomePanel.Inicio;
 
-public class Buscaminas extends JFrame {
-    private Tablero tablero;
-    private JRadioButton rbBFS, rbDFS;
-
-    public Buscaminas() { 
-        setTitle("Buscaminas");
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-
-        // Pedir al usuario las dimensiones del tablero
-        int filas = Integer.parseInt(JOptionPane.showInputDialog("Número de filas (3-10):"));
-        int columnas = Integer.parseInt(JOptionPane.showInputDialog("Número de columnas (3-10):"));
-        int minas = Integer.parseInt(JOptionPane.showInputDialog("Número de minas:"));
-
-        // Validar que las dimensiones estén dentro del rango permitido
-        if (filas < 3 || filas > 10 || columnas < 3 || columnas > 10) {
-            JOptionPane.showMessageDialog(null, "Las filas y columnas deben estar entre 3 y 10.");
-            return;
-        }
-
-        // Validar que el número de minas no exceda el número de casillas
-        if (minas > filas * columnas) {
-            JOptionPane.showMessageDialog(null, "El número de minas no puede ser mayor que el número de casillas.");
-            return;
-        }
-        // Selector de algoritmo
-        JPanel panelBusqueda = new JPanel();
-        rbBFS = new JRadioButton("BFS", true);
-        rbDFS = new JRadioButton("DFS");
-        ButtonGroup grupo = new ButtonGroup();
-        grupo.add(rbBFS);
-        grupo.add(rbDFS);
-
-        rbBFS.addActionListener(e -> tablero.setUsarBFS(true));
-        rbDFS.addActionListener(e -> tablero.setUsarBFS(false));
-
-        panelBusqueda.add(rbBFS);
-        panelBusqueda.add(rbDFS);
-        add(panelBusqueda, BorderLayout.NORTH);
-        
-        // Añadir el contador de banderas
-        JLabel lblBanderas = new JLabel("Banderas maximas: " + minas);
-        panelBusqueda.add(lblBanderas);
-
-    add(panelBusqueda, BorderLayout.NORTH);
-
-
-        // Crear el tablero
-        tablero = new Tablero(filas, columnas, minas);
-
-        // Añadir el tablero a la ventana en la posición CENTER
-        add(tablero, BorderLayout.CENTER);
-
-        // Mostrar la ventana
-        setVisible(true);
-        
-    }
-    
-    
+public class Buscaminas {
 
     public static void main(String[] args) {
-        // Ejecutar la interfaz gráfica en el hilo de eventos de Swing
-        SwingUtilities.invokeLater(() -> new Buscaminas());
-    }
+        
     
+    Inicio inicio =new Inicio();
+    inicio.setVisible(true);
+    inicio.setLocationRelativeTo(null);
+           
+ 
+    
+    }
 }
+    
+   
+    
